91acb2a99afddd172e7383b3f728b5033724663b
Arjen <arjenmeer@hotmail.nl>
2020-12-17 15:02:52 +0100

    changes main pom to support the new modules

---
+++ osgp/protocol-adapter-opc-ua/osgp-protocol-adapter-opc-ua/src/main/java/org/opensmartgridplatform/adapter/protocol/opc/ua/application/config/main.java
@@ -0,0 +1,7 @@
+package org.opensmartgridplatform.adapter.protocol.opc.ua.application.config;
+
+class main {
+    public static void main(final String[] args) {
+        System.out.println("pest?");
+    }
+}
